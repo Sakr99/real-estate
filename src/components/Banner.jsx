13f5c8639acb027e1097077c42287ae88e417dec
Abdelrahman Sakr <abdelrahmansakr199@gmail.com>
2025-07-22 21:05:44 +0300

    edit projects page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,8 +6,8 @@ const Banner = () => {
   return (
     <section className="mt-2 overflow-hidden md:h-screen bg-[url(https://www.whitecase.com/sites/default/files/styles/original_image/public/images/hero/2024/03/2024-real-estate-market-sentiment-survey-hero.jpg?itok=ggRx37lY)] bg-cover bg-center">
       <div className="bg-black/60 w-full h-full flex flex-col md:flex-row items-center  justify-center  md:justify-between px-6 md:px-16 py-10 md:py-20">
-        <div className="  text-center md:text-left max-w-xl">
-          <h2 className="text-4xl text-white md:text-5xl mb-4">
+        <div className="  text-center max-w-xl">
+          <h2 className="text-4xl  text-white md:text-5xl mb-4">
             Sakr Real Estate
           </h2>
           <hr />
@@ -22,7 +22,7 @@ const Banner = () => {
             href="https://wa.me/201000219106"
             rel="noreferrer"
             target="_blank"
-            className="flex  hover:bg-green-600 rounded-full items-center justify-center  gap-2 mt-8 px-6 py-2 bg-slate-500 text-white transition"
+            className="flex hover:bg-green-600 rounded-full items-center justify-center  gap-2 mt-8 px-6 py-2 bg-slate-500 text-white transition"
           >
             <FaWhatsapp className=" size-6" />
              احجز الان عبر الواتساب
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,28 @@
-
 const Projects = () => {
   return (
-    <div
-      
-      className="  grid grid-cols-4 gap-4 p-10 items-center bg-gray-500"
-    >
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 p-4 items-center bg-white">
       <img
-        src="https://realestate.eg/storage/developers/F9WZJXdR213lgKUF.jpg"
+        src="https://pioneerproperty.net/wp-content/uploads/2021/08/%D8%B4%D8%B1%D9%83%D8%A9-%D8%A8%D8%A7%D9%84%D9%85-%D9%87%D9%8A%D9%84%D8%B2-%D8%A7%D9%84%D8%B9%D9%82%D8%A7%D8%B1%D9%8A%D8%A9.jpg"
         alt="Project"
+        className="w-full h-32 object-cover"
       />
       <img
-        src="https://waterfront.the8-realestate.com/wp-content/uploads/2022/12/Layer-5.png"
+        src="https://media.osoulmisrmagazine.com/2025/2/large/2767373462624202502110354375437.jpg"
         alt="Project"
+        className="w-full h-32 object-cover"
       />
       <img
-        src="https://skyline.the8-realestate.com/wp-content/uploads/2023/01/Logo-768x202.png"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5L1qXeoMsgumRrWflgDfY1vb9Hjvb7pZtcA&s"
         alt="Project"
+        className="w-full h-32 object-cover"
       />
       <img
-        src="https://sawari.the8-realestate.com/wp-content/uploads/2023/01/Sawari-Logo-White.png"
+        src="https://developers-eg.com/wp-content/uploads/2022/07/%D8%A7%D9%83%D8%A7%D9%85-%D8%A7%D9%84%D8%B1%D8%A7%D8%AC%D8%AD%D9%8A-%D9%84%D9%84%D8%AA%D8%B7%D9%88%D9%8A%D8%B1-%D8%A7%D9%84%D8%B9%D9%82%D8%A7%D8%B1%D9%8A.jpg"
         alt="Project"
+        className="md:w-full h-32 "
       />
     </div>
   );
-}
+};
 
-export default Projects
+export default Projects;
